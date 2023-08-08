d530f5dfa18d4d811f99f3ebd5ba6b3cd32ba6f4
Shyam Sharma <shyamsharma.sharma797@gmail.com>
2023-08-08 14:09:26 -0400

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
+import { FormEvent, useRef, useState } from "react";
 import Display from "./components/Display";
 
 function App() {
   const obj: any = "Hello World";
+
+  const nameRef = useRef<any>();
+
+  const [data, setData] = useState<any>("World"); //useState hook
+  const handleSbmit = () => {
+    console.log("Changed the heading");
+    console.log(data);
+    setData("Hello World");
+  };
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log(nameRef.current.value);
+    setData(nameRef.current.value);
+  };
   return (
     <>
-      <h1>{obj}</h1>
-      <Display />
+      <h1>{`Hello ${data}`}</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" ref={nameRef} />
+        <button className="btn btn-success ms-4">Change Heading</button>
+      </form>
+      <Display onSubmit={handleSbmit} value={obj} />
     </>
   );
 }
--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,6 +1,14 @@
+import { FormEvent, useRef } from "react";
 import NavBar from "./NavBar";
 
-const Display = () => {
+const Display = (props: any) => {
+  const nameRef = useRef<any>();
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>): void {
+    event.preventDefault();
+    console.log(nameRef.current.value);
+  }
+
   return (
     <div>
       <h1></h1>
@@ -11,6 +19,19 @@ const Display = () => {
         excepturi temporibus debitis totam consectetur eveniet ipsa harum.
         Obcaecati illum repudiandae natus!
       </p>
+      <button className="btn btn-primary" onClick={props.onSubmit}>
+        Change Heading
+      </button>
+
+      <form className="form-control mt-5" onSubmit={handleSubmit}>
+        <label htmlFor="name" className="form-label">
+          Name :
+        </label>
+        <input type="text" id="name" ref={nameRef} />
+        <button type="submit" className="btn btn-info ms-3">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
